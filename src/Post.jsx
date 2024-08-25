a8483544c9044422b89298f3197f98f6f54404d7
sudohelal <helaluddin40@outlook.com>
2024-08-25 12:53:56 +0600

    dispaly data to UI

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,12 +1,11 @@
 import './post.css'
-export default function Post({post}){
-    console.log(post)
-    if(!post){
+export default function Post({ post }) {
+    if (!post) {
         return null;
     }
-    const {title, id, userId, body} = post;
-    
-    return(
+    const { title, id, userId, body } = post;
+
+    return (
         <div className="box">
             <h3>Title: {title}</h3>
             <p><small>userId:{userId} </small></p>
